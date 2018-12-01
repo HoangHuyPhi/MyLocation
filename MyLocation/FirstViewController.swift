b85cf9977cd590228424913f2ef8f3cb61b1cc12
Huy Phi Hoang <phihoanghuy@phis-macbook-pro.local>
2018-12-01 14:32:25 +0700

    Initial Commit

---
+++ MyLocation/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocation
+//
+//  Created by Phi Hoang Huy on 12/1/18.
+//  Copyright © 2018 Phi Hoang Huy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyLocation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocation
+//
+//  Created by Phi Hoang Huy on 12/1/18.
+//  Copyright © 2018 Phi Hoang Huy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
